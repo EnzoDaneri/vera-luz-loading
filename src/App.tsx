21d107a898bb0a46d2db7b4e25845b58315ab81e
EnzoDaneri <enzodaneri@hotmail.com>
2021-07-24 19:20:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [loading, setIsLoding] = useState(true);
+
+  const spinner = () => {
+    setIsLoding(!loading);
+  };
+
+  return (
+    <>
+      <h1>vera luz</h1>
+      <div className="container">
+        {loading && <div className="spinner"></div>}
+      </div>
+
+      <button onClick={spinner}>True - False</button>
+    </>
+  );
+}
+
+export default App;
